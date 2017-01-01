1a77221a4f35778deee9007d39823a63dfff1524
Skyler Brandt <brandtsa@live.com>
2017-01-01 15:49:42 -0500

    hyperlinked matches

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,14 @@
 function sidebar_open() {
   document.getElementById('main').style.marginLeft = '230px';
   document.getElementById('titlebar').style.marginLeft = '220px';
-  document.getElementById('sidebar').style.width = '220px';
-  document.getElementById('sidebar').style.display = 'block';
-  document.getElementById('opennavbtn').style.display = 'none';
+  document.getElementById('side_nav').style.width = '220px';
+  document.getElementById('side_nav').style.display = 'block';
+  document.getElementById('open_nav').style.display = 'none';
 }
 
 function sidebar_close() {
   document.getElementById('main').style.marginLeft = '40px';
   document.getElementById('titlebar').style.marginLeft = '0';
-  document.getElementById("sidebar").style.display = 'none';
-  document.getElementById("opennavbtn").style.display = 'inline-block';
+  document.getElementById("side_nav").style.display = 'none';
+  document.getElementById("open_nav").style.display = 'inline-block';
 }
