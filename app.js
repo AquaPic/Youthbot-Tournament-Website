d65278be24a55971195c6b7fd52802d284f4282d
Skyler Brandt <brandtsa@live.com>
2016-12-31 20:54:40 -0500

    work on sidenav

--- app.js
+++ app.js
@@ -49,11 +49,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/*
 db.connect(function(err) {
   if(err)
     next(err)
 
   console.log('Successfully connected to SQL server')
 })
+*/
 
 module.exports = app;
