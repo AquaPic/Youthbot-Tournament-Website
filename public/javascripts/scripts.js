bf5d4309d56c218b377e2e568522d5bb7d66a91b
Skyler Brandt <brandtsa@live.com>
2017-01-01 17:30:26 -0500

    work on new header

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,14 @@
 function sidebar_open() {
   document.getElementById('main').style.marginLeft = '230px';
-  document.getElementById('titlebar').style.marginLeft = '220px';
+  document.getElementById('header').style.zIndex = 'auto';
+  document.getElementById('header_title').style.marginLeft = '200px';
   document.getElementById('side_nav').style.width = '220px';
   document.getElementById('side_nav').style.display = 'block';
-  document.getElementById('open_nav').style.display = 'none';
 }
 
 function sidebar_close() {
   document.getElementById('main').style.marginLeft = '40px';
-  document.getElementById('titlebar').style.marginLeft = '0';
+  document.getElementById('header').style.zIndex = '1';
+  document.getElementById('header_title').style.marginLeft = '0px';
   document.getElementById("side_nav").style.display = 'none';
-  document.getElementById("open_nav").style.display = 'inline-block';
 }
