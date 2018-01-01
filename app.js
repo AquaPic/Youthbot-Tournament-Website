e4043ca55cb90ef5172306cb4e2793d5a412e5ab
Skyler Brandt <sbrandt@vps110157.vps.ovh.ca>
2018-01-01 12:21:36 -0500

    connecting to production server

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-db.connect(db.MODE_TEST, false, function(err) {
+db.connect(db.MODE_PRODUCTION, false, function(err) {
   if(err)
     next(err)
 
